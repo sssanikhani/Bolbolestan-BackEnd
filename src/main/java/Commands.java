938f70fa991524df0566f9ed60f628e7a5c6b322
RezaQavi <rezaqavi1378@gmail.com>
2021-02-27 16:22:33 +0330

    add junit move "commandHandle" and "commandReader" functions to class Commands create first simple test :/

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -4,35 +4,69 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Commands {
 
-//    private static ArrayList<Course> allCourses = new ArrayList<Course>();
     public static HashMap<String, Offer> allOffers = new HashMap<String, Offer>();
     public static HashMap<String, ArrayList<Offer>> allOffersOfACourse = new HashMap<String, ArrayList<Offer>>();
-    //    private static ArrayList<Student> allStds = new ArrayList<Student>();
     public static HashMap<String, Student> allStds = new HashMap<String, Student>();
 
     public Commands() {
     }
 
-    private static void createOutputJson(boolean b, String data, String s) throws JsonProcessingException {
+    static String commandReader() {
+        Scanner reader = new Scanner(System.in);
+        return reader.nextLine();
+    }
+    void commandHandler(String[] cmdp) throws IOException, ParseException {
+        switch (cmdp[0]) {
+            case "addOffering":
+                System.out.println(addCourse(cmdp[1]));
+                break;
+            case "addStudent":
+                addStudent(cmdp[1]);
+                break;
+            case "getOffering":
+                getOffer(cmdp[1]);
+                break;
+            case "getOfferings":
+                getOffers(cmdp[1]);
+                break;
+            case "addToWeeklySchedule":
+                addCourseToSch(cmdp[1]);
+                break;
+            case "removeFromWeeklySchedule":
+                removeCourseFromSch(cmdp[1]);
+                break;
+            case "getWeeklySchedule":
+                getWeeklySch(cmdp[1]);
+                break;
+            case "finalize":
+                // TODO
+                break;
+        }
+    }
+
+
+
+    public static String createOutputJson(boolean b, String data, String s) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode message = mapper.createObjectNode();
         message.put("success", b);
         message.put(data, s);
         String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(message);
-        System.out.println(json);
+//        System.out.println(json);
+        return json;
     }
 
-    public static void addCourse(String js) throws IOException {
+     public static String  addCourse(String js) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         Offer newCourse = mapper.readValue(js, Offer.class);
-//        allCourses.add(newCourse);
         allOffers.put(newCourse.getCode(), newCourse);
-//        allOffersOfACourse.put(newCourse.getName(), newCourse);
         if(allOffersOfACourse.get(newCourse.getName()) != null) {
             allOffersOfACourse.get(newCourse.getName()).add(newCourse);
         } else  {
@@ -40,13 +74,12 @@ public class Commands {
             allOffersOfACourse.get(newCourse.getName()).add(newCourse);
 
         }
-        createOutputJson(true, "data", "OfferingAdded");
+        return(createOutputJson(true, "data", "OfferingAdded"));
     }
 
     public static void addStudent(String js) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         Student newStd = mapper.readValue(js, Student.class);
-//        allStds.add(newStd);
         allStds.put(newStd.getStudentId(), newStd);
         createOutputJson(true, "data", "StudentAdded");
     }
@@ -65,19 +98,6 @@ public class Commands {
         } else {
             createOutputJson(false, "error", "OfferingNotFound");
         }
-
-//
-//        Course fCourse = allCourses.stream().filter(course_t -> courseCode.equals(course_t.getCode())).findAny().orElse(null);
-//        if(fCourse != null){
-//            for (Student s : allStds){
-//                if(s.getStudentId().equals(stdId)){
-//                    s.addCourseToList(fCourse);
-//                    createOutputJson(true, "data", "CourseAdded");
-//                }
-//            }
-//        }else {
-//            createOutputJson(false, "error", "OfferingNotFound");
-//        }
     }
 
     public static void removeCourseFromSch(String js) throws IOException {
@@ -97,21 +117,6 @@ public class Commands {
         } else {
             createOutputJson(false, "error", "OfferingNotFound");
         }
-
-//        Course fCourse = allCourses.stream().filter(course_t -> courseCode.equals(course_t.getCode())).findAny().orElse(null);
-//        if(fCourse != null){
-//            for (Student s : allStds){
-//                if(s.getStudentId().equals(stdId)) {
-//                    if (s.removeCourseFromList(fCourse)) {
-//                        createOutputJson(true, "data", "OfferingRemoved");
-//                    }else {
-//                        createOutputJson(false, "error", "OfferingNotFound");
-//                    }
-//                }
-//            }
-//        }else {
-//            createOutputJson(false, "error", "OfferingNotFound");
-//        }
     }
 
     public static void getOffer(String js) throws IOException {
@@ -119,7 +124,6 @@ public class Commands {
         JsonNode jn = mapper.readTree(js);
         String stdId = jn.get("StudentId").asText();
         String courseCode = jn.get("code").asText();
-//        Course fCourse = allCourses.stream().filter(course_t -> courseCode.equals(course_t.getCode())).findAny().orElse(null);
         String message = "\"data\": ";
         if(allOffers.get(courseCode) != null) {
             message += mapper.writerWithView(View.normal.class).writeValueAsString(allOffers.get(courseCode));
@@ -127,7 +131,6 @@ public class Commands {
         } else {
             createOutputJson(false, "error", "OfferingNotFound");
         }
-
     }
 
     public static void getOffers(String js) throws IOException {
@@ -144,50 +147,9 @@ public class Commands {
         JsonNode jn = mapper.readTree(js);
         String stdId = jn.get("StudentId").asText();
         String message = "\"data\": {\"weeklySchdule\": ";
-
-//        Student fStd = allStds.stream().filter(std_f -> stdId.equals(std_f.getStudentId())).findAny().orElse(null);
-
         message += mapper.writerWithView(View.weeklySch.class).writeValueAsString(allStds.get(stdId).getWeeklyCourses().values());
         message += "}";
         System.out.println(message);
     }
-
-//    public static void finalizeSch(String js) throws JsonProcessingException, ParseException {
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode jn = mapper.readTree(js);
-//        String stdId = jn.get("StudentId").asText();
-//        Student fStd = allStds.stream().filter(std_f -> stdId.equals(std_f.getStudentId())).findAny().orElse(null);
-//        ArrayList<Course> stdCourses = fStd.getWeeklyCourses();
-//        if(stdCourses == null) {
-//            createOutputJson(false, "error", "Course list is empty.");
-//            return;
-//        }
-//        int units = 0;
-//        for (int i = 0; i< stdCourses.size(); i++) {
-//            units += stdCourses.get(i).getUnits();
-//            for (int j = i+1; j< stdCourses.size(); j++) {
-//                checkClassTime(stdCourses.get(i).getClassTime(), stdCourses.get(j).getClassTime());
-//            }
-//        }
-//    }
-//
-//    public static void checkClassTime(CourseClassTime c1, CourseClassTime c2) throws ParseException {
-//        System.out.println("Here");
-//        int m, n;
-//        for(m = 0; m< c1.getDays().size(); m++) {
-//            for (n = 0; n< c2.getDays().size(); n++) {
-//                if(c1.getDays().get(m) == c2.getDays().get(n)){
-//                    System.out.println(c1.getDays().get(m));
-//                    SimpleDateFormat format = new SimpleDateFormat("HH:mm-HH:mm");
-//                    Date c1Date = format.parse(c1.getTime());
-//                    Date c2Date = format.parse(c2.getTime());
-//                    System.out.println(c1Date);
-//                    System.out.println(c2Date);
-//                }
-//            }
-//        }
-//
-//    }
-
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,47 +8,12 @@ import java.util.ArrayList;
 
 public class Main {
 
-
-    static String commandReader() {
-        Scanner reader = new Scanner(System.in);
-        return reader.nextLine();
-    }
-    static void commandHandler(String[] cmdp) throws IOException, ParseException {
-        switch (cmdp[0]) {
-            case "addOffering" :
-                Commands.addCourse(cmdp[1]);
-                break;
-            case "addStudent":
-                Commands.addStudent(cmdp[1]);
-                break;
-            case "getOffering":
-                Commands.getOffer(cmdp[1]);
-                break;
-            case "getOfferings":
-                Commands.getOffers(cmdp[1]);
-                break;
-            case "addToWeeklySchedule":
-                Commands.addCourseToSch(cmdp[1]);
-                break;
-            case "removeFromWeeklySchedule":
-                Commands.removeCourseFromSch(cmdp[1]);
-                break;
-            case "getWeeklySchedule":
-                Commands.getWeeklySch(cmdp[1]);
-                break;
-            case "finalize":
-//                Commands.finalizeSch(cmdp[1]);
-                // TODO
-                break;
-        }
-
-    }
-
     public static void main(String[] args) throws IOException, ParseException {
+        Commands command = new Commands();
         while (true) {
-            String cmd = commandReader();
+            String cmd = command.commandReader();
             String[] cmdParts = cmd.split(" ", 2);
-            commandHandler(cmdParts);
+            command.commandHandler(cmdParts);
         }
     }
 }
