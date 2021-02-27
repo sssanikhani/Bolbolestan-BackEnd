4e56694f76bac64c19b6cb4ff057c7d767407e05
Sajjad <sssanikhani@gmail.com>
2021-02-28 02:17:13 +0330

    feat: add 'numberChosenUnits' field to Student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -4,6 +4,7 @@ public class Student {
     private String studentId;
     private String name;
     private String enteredAt;
+    private int numberChosenUnits = 0;
     // private ArrayList<Course> weeklyCourses = new ArrayList<Course>();
     private HashMap<String, Offer> weeklyCourses = new HashMap<String, Offer>();
 
@@ -38,17 +39,26 @@ public class Student {
         return weeklyCourses;
     }
 
-    public void setWeeklyCourses(HashMap<String, Offer> weeklyCourses) {
-        this.weeklyCourses = weeklyCourses;
+    public int getNumberChosenUnits() {
+        return numberChosenUnits;
     }
+    // public void setWeeklyCourses(HashMap<String, Offer> weeklyCourses) {
+    //     this.weeklyCourses = weeklyCourses;
+    // }
 
     public void addCourseToList(Offer c) {
         this.weeklyCourses.put(c.getCode(), c);
+        this.numberChosenUnits += c.getUnits();
         // System.out.println(this.weeklyCourses);
     }
 
-    public Offer removeCourseFromList(Offer c) {
+    public Offer removeCourseFromList(String c) throws Exception{
         // this.weeklyCourses.remove(c);
-        return this.weeklyCourses.remove(c.getCode());
+        Offer offer = this.weeklyCourses.get(c);
+        if (offer == null) {
+            throw new Exceptions.OfferingNotFound();
+        }
+        this.numberChosenUnits -= offer.getUnits();
+        return this.weeklyCourses.remove(c);
     }
 }
