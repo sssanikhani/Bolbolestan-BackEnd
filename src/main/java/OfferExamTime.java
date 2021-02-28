a34defcfc5c098de37547ac22199e37bbe9b0ada
Sajjad <sssanikhani@gmail.com>
2021-02-28 14:09:15 +0330

    feat: add method to check time collision with another object

--- src/main/java/OfferExamTime.java
+++ src/main/java/OfferExamTime.java
@@ -1,7 +1,12 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class OfferExamTime {
     String start;
     String end;
+    Date startDate;
+    Date endDate;
 
     public OfferExamTime() {
     }
@@ -10,15 +15,56 @@ public class OfferExamTime {
         return start;
     }
 
-    public void setStart(String start) {
+    public void setStart(String start) throws ParseException {
         this.start = start;
+        SimpleDateFormat frm = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        this.startDate = frm.parse(start);
+    }
+
+    public Date getStartDate() {
+        return startDate;
     }
 
     public String getEnd() {
         return end;
     }
 
-    public void setEnd(String end) {
+    public void setEnd(String end) throws ParseException {
         this.end = end;
+        SimpleDateFormat frm = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        this.endDate = frm.parse(start);
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public boolean hasCollision(OfferExamTime t) throws ParseException {
+        Date s1 = this.startDate;
+        Date s2 = t.getStartDate();
+        Date e1 = this.endDate;
+        Date e2 = t.getEndDate();
+
+        // (S1, _________E1)
+        // _____(S2, E2)
+        if (s1.compareTo(s2) <= 0 && e1.compareTo(e2) >= 0)
+            return true;
+
+        // _____(S1, E1)
+        // (S2, _________E2)
+        if (s1.compareTo(s2) >= 0 && e1.compareTo(e2) <= 0)
+            return true;
+
+        // (S1, E1)
+        // ____(S2, E2)
+        if (s1.compareTo(s2) < 0 && e1.compareTo(s2) > 0)
+            return true;
+
+        // ____(S1, E1)
+        // (S2, E2)
+        if (s1.compareTo(s2) > 0 && s1.compareTo(e2) < 0)
+            return true;
+
+        return false;
     }
 }
