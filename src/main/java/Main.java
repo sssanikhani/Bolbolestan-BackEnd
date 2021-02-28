d8bfbc656dce7bd69822fd2258dbaf153261a8f8
RezaQavi <rezaqavi1378@gmail.com>
2021-02-28 23:19:16 +0330

    redesign output to json format string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,39 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.HashMap;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Main {
+    public Main() {
+    }
+
+    public static void parseCmd(String cmd) throws JsonProcessingException {
+        String[] cmdParts = cmd.split("\\s+", 2);
+        ObjectMapper mapper = new ObjectMapper();
+//        HashMap<String, Object> res = new HashMap<String, Object>();
+//         ObjectMapper mapper = new ObjectMapper();
+//         ObjectNode res = mapper.createObjectNode();
+        String result = "{\n\t";
+        try {
+            String data = CommandHandler.performCommand(cmdParts);
+            result += "\"success\" : true\n\t";
+            result += "\"data\" : " + data ;
+            result += "\n}";
+//            res.put("data", data);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            result += "\"success\" : false\n\t";
+            result += "\"error\" : " + error ;
+            result += "\n}";
+//            res.put("success", false);
+//            res.put("error", error);
+        }
+//        String response = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
+        System.out.println(result);
+    }
+
 
     public static void main(String[] args) throws IOException, ParseException {
         while (true) {
@@ -14,19 +44,7 @@ public class Main {
                 System.out.println("Usage: <Command> <JSON Form Data>");
                 continue;
             }
-            ObjectMapper mapper = new ObjectMapper();
-            HashMap<String, Object> res = new HashMap<String, Object>();
-            try {
-                String data = CommandHandler.performCommand(cmdParts);
-                res.put("success", true);
-                res.put("data", data);
-            } catch (Exception e) {
-                String error = e.getMessage();
-                res.put("success", false);
-                res.put("error", error);
-            }
-            String response = mapper.writeValueAsString(res);
-            System.out.println(response);
+            parseCmd(cmd);
         }
     }
 }
