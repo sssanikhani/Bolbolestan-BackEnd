ec99b003a4c70bca5fc76436690d0740baf22043
RezaQavi <rezaqavi1378@gmail.com>
2021-03-01 00:25:31 +0330

    test file rename

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,34 +1,30 @@
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class test1 {
 
+    // CommandHandler cmdHandler = new CommandHandler();
     Main mainTest = new Main();
 
     @Before
     public void setup() throws Exception {
-        Scanner scanner = new Scanner(new File("src/test/resources/test1.txt"));
+        Scanner scanner = new Scanner(new File("src/test/resources/test3.txt"));
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             mainTest.parseCmd(line);
         }
     }
 
-
     @Test
-    public void MaximumUnitsTest() throws IOException {
-        String cmd = "finalize {\"StudentId\": \"810197559\"}";
+    public void testAddOffer() throws IOException {
+        String cmd = "{\"code\": \"81015960\", \"name\": \"AI\" , \"instructor\" : \"Ramtin\", \"units\": 3, \"capacity\" : 60, \"prerequisites\": [\"Mabani\"], \"classTime\" : {\"days\": [\"Sat\", \"Mon\"], \"time\": \"16-17:30\"}, \"examTime\": {\"start\":  \"2021-9-01T08:00:00\", \"end\":  \"2021-9-01T08:00:00\"}}";
         assertEquals(true, true);
     }
-
 
 }
