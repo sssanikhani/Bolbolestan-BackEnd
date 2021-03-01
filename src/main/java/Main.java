0a358953c0b25a0af4e2870ce4550a6c0dd8d90e
RezaQavi <rezaqavi1378@gmail.com>
2021-03-01 10:41:16 +0330

    change parseCmd function to return a String

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,29 @@
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.HashMap;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Main {
     public Main() {
     }
 
-    public static void parseCmd(String cmd) throws JsonProcessingException {
+    public static String parseCmd(String cmd) throws JsonProcessingException {
         String[] cmdParts = cmd.split("\\s+", 2);
         ObjectMapper mapper = new ObjectMapper();
-        // HashMap<String, Object> res = new HashMap<String, Object>();
-        // ObjectMapper mapper = new ObjectMapper();
-        // ObjectNode res = mapper.createObjectNode();
         String result = "{\n\t";
         try {
             String data = CommandHandler.performCommand(cmdParts);
-            result += "\"success\" : true\n\t";
+            result += "\"success\" : true,\n\t";
             result += "\"data\" : \"" + data + "\"";
             result += "\n}";
-            // res.put("data", data);
         } catch (Exception e) {
             String error = e.getMessage();
-            result += "\"success\" : false\n\t";
+            result += "\"success\" : false,\n\t";
             result += "\"error\" : \"" + error + "\"";
             result += "\n}";
-            // res.put("success", false);
-            // res.put("error", error);
         }
-        // String response =
-        // mapper.writerWithDefaultPrettyPrinter().writeValueAsString(res);
-        System.out.println(result);
+        return result;
     }
 
     public static void main(String[] args) throws IOException, ParseException {
