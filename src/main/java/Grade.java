6618c614456239c79527c2906327d74a3fb69508
RezaQavi <rezaqavi1378@gmail.com>
2021-03-03 19:12:12 +0330

    create grade class for store grades

---
+++ src/main/java/Grade.java
@@ -0,0 +1,22 @@
+
+
+public class Grade {
+    private String code;
+    private int grade;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
