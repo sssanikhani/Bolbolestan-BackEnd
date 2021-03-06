0f160d48c400c879cceb83e510ed665b7392c5d2
Sajjad <sssanikhani@gmail.com>
2021-03-06 12:45:00 +0330

    feat: add courses handler

--- src/main/java/Handlers.java
+++ src/main/java/Handlers.java
@@ -1,49 +1,69 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.javalin.http.Handler;
 
 public class Handlers {
 
     public static Handler courses = ctx -> {
-        String html = "";
-        ctx.html(html);
+
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        ArrayList<HashMap<String, Object>> offeringsDataList = new ArrayList<>();
+        data.put("courses", offeringsDataList);
+
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<Offering> offeringsList = DataBase.OfferingManager.getAll();
+        for (Offering o : offeringsList) {
+            HashMap<String, Object> oData = mapper.convertValue(o, HashMap.class);
+            offeringsDataList.add(oData);
+        }
+
+        // String html = HtmlRenderer.renderCoursesPage(data);
+        // ctx.html(html);
     };
 
     public static Handler studentProfile = ctx -> {
-        String html = "";
-        ctx.html(html);
+
+        // String html = HtmlRenderer.renderStudentProfilePage(data);
+        // ctx.html(html);
     };
-    // app.get("/courses", Handlers.getCourses);
-    // app.get("/profile/:studentId",  Handlers.stdProfile);
-    // app.get("/course/:courseId/:classCode", Handlers.);
-    // app.get("/change_plan/:studentId", Handlers.);
-    // app.get("/plan/:studentId", Handlers.);
-    // app.get("/submit/:studentId", Handlers.);
-    // app.get("/submit_ok", Handlers.);
-    // app.get("/submit_failed", Handlers.);
 
     public static Handler singleCourse = ctx -> {
 
+        // String html = HtmlRenderer.renderSingleCoursePage(data);
+        // ctx.html(html);
     };
 
     public static Handler changePlan = ctx -> {
 
+        // String html = HtmlRenderer.renderChangePlanPage(data);
+        // ctx.html(html);
     };
 
     public static Handler plan = ctx -> {
 
+        // String html = HtmlRenderer.renderPlanPage(data);
+        // ctx.html(html);
     };
 
     public static Handler submit = ctx -> {
 
+        // String html = HtmlRenderer.renderSubmitPage(data);
+        // ctx.html(html);
     };
 
     public static Handler okSubmit = ctx -> {
 
+        // String html = HtmlRenderer.renderOkSubmitPage(data);
+        // ctx.html(html);
     };
 
     public static Handler failSubmit = ctx -> {
 
+        // String html = HtmlRenderer.renderFailSubmitPage(data);
+        // ctx.html(html);
     };
 
 }
