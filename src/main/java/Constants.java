cc526cc24c7475436c33dcf0b448ebbd05d51c1a
Sajjad <sssanikhani@gmail.com>
2021-03-08 23:56:36 +0330

    feat: add all static page files path to Constants module

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -5,4 +5,15 @@ public class Constants {
 
     final static int SERVER_PORT = 7000;
 
+    public static final String PAGE_NOT_FOUND_PATH = "src/main/resources/templates/404.html";
+    public static final String PAGE_BLANK_PATH = "src/main/resources/templates/blank.html";
+    public static final String PAGE_CHANGE_PLAN_PATH = "src/main/resources/templates/change_plan.html";
+    public static final String PAGE_SINGLE_COURSE_PATH = "src/main/resources/templates/course.html";
+    public static final String PAGE_COURSES_PATH = "src/main/resources/templates/courses.html";
+    public static final String PAGE_PLAN_PATH = "src/main/resources/templates/plan.html";
+    public static final String PAGE_PROFILE_PATH = "src/main/resources/templates/profile.html";
+    public static final String PAGE_SUBMIT_PATH = "src/main/resources/templates/submit.html";
+    public static final String PAGE_SUBMIT_OK_PATH = "src/main/resources/templates/submit_ok.html";
+    public static final String PAGE_SUBMIT_FAILED_PATH = "src/main/resources/templates/submit_failed.html";
+
 }
