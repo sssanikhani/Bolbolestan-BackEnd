dfebbbda116701dd5d1229a61681b49d761e08aa
Sajjad <sssanikhani@gmail.com>
2021-03-18 21:01:45 +0330

    change Handlers.startup() body to ContextListener initializer

--- src/main/java/servlets/ContextListener.java
+++ src/main/java/servlets/ContextListener.java
@@ -3,13 +3,20 @@ package servlets;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import models.logic.Handlers;
+import models.logic.DataBase;
 
 public class ContextListener implements ServletContextListener {
     
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        Handlers.startup();
+        try {
+			System.out.println("Trying to retrieve data from external DataBase...");
+			DataBase.OfferingManager.updateFromExternalServer();
+			DataBase.StudentManager.updateFromExternalServer();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ERROR: failed to connect with external server");
+		}
     }
 
     @Override
