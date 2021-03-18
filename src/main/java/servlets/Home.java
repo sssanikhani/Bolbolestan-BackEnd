02df9c214f12ea862f64dcbb7fcf14966b0d93fb
Sajjad <sssanikhani@gmail.com>
2021-03-18 15:45:44 +0330

    fix: organize imports in Home Servlet

--- src/main/java/servlets/Home.java
+++ src/main/java/servlets/Home.java
@@ -1,7 +1,6 @@
 package servlets;
 
-import models.logic.DataBase;
-import models.logic.Handlers;
+import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,7 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import models.logic.DataBase;
 
 
 @WebServlet(name = "HomeServlet", value = "")
