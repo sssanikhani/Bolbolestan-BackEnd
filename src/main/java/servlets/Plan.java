32592b874aa13a41605ee7e7aeaeef75fffff33f
Sajjad <sssanikhani@gmail.com>
2021-03-18 18:58:22 +0330

    feat: refactor plan.jsp and delete plan.txt and planBody in Constants module

--- src/main/java/servlets/Plan.java
+++ src/main/java/servlets/Plan.java
@@ -1,11 +1,8 @@
 package servlets;
 
-import models.statics.Constants;
-import models.logic.DataBase;
-import models.logic.Handlers;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,53 +10,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+
+import models.logic.DataBase;
+import models.logic.Handlers;
 
 @WebServlet(name = "PlanServlet", value = "/plan")
 public class Plan extends HttpServlet {
 
-    public String createBody(ArrayList<HashMap<String, Object>> courses) throws IOException {
-        Document reportDoc;
-        // TODO remove this constant and add plan.txt file path
-        reportDoc = Jsoup.parse(Constants.planbody, "UTF-8");
-        Element table = reportDoc.getElementsByTag("table").last();
-        String[] weekDays = { "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday" };
-        for (HashMap<String, Object> entry : courses) {
-            HashMap<String, Object> classTimeData = (HashMap<String, Object>) entry.get("classTime");
-            String name = (String) entry.get("name");
-            ArrayList<String> days = (ArrayList<String>) classTimeData.get("days");
-            String time = (String) classTimeData.get("time");
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		if (DataBase.getLoggedInUserId() == null) {
+			response.sendRedirect(request.getContextPath() + "/login");
+		} else {
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/plan.jsp");
+			HashMap<String, Object> student = Handlers.getStudentData(
+				DataBase.getLoggedInUserId()
+			);
+			ArrayList<HashMap<String, Object>> submittedOfferings = (ArrayList<HashMap<String, Object>>) student.get(
+				"lastPlan"
+			);
+			request.setAttribute("courses", submittedOfferings);
+			request.setAttribute("student", student);
+			requestDispatcher.forward(request, response);
+		}
+	}
 
-            for (String weekDay : weekDays) {
-                if (days.contains(weekDay)) {
-                    Element dayElem = table.getElementById(weekDay);
-                    Element cell = dayElem.getElementById(time);
-                    cell.text(name);
-                }
-            }
-        }
-        return reportDoc.html();
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (DataBase.getLoggedInUserId() == null) {
-            response.sendRedirect(request.getContextPath()+ "/login");
-        } else {
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/plan.jsp");
-            HashMap<String, Object> student = Handlers.getStudentData(DataBase.getLoggedInUserId());
-            ArrayList<HashMap<String, Object>> selectedCourses =
-                    (ArrayList<HashMap<String, Object>>) student.get("lastPlan");
-            request.setAttribute("planBody", createBody(selectedCourses));
-            request.setAttribute("student", student);
-            requestDispatcher.forward(request, response);
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {}
 }
--- src/main/webapp/plan.jsp
+++ src/main/webapp/plan.jsp
@@ -18,12 +18,47 @@
     </style>
 </head>
 <body>
-<% HashMap<String , Object> student = (HashMap<String, Object>) request.getAttribute("student");%>
-<ul>
-    <a href="/">Home</a>
-    <li id="code">Student Id: <%= student.get("id") %> </li>
-</ul>
-<br>
-    <%= request.getAttribute("planBody")%>
+    <% HashMap<String , Object> student = (HashMap<String, Object>) request.getAttribute("student");%>
+    <ul>
+        <a href="/">Home</a>
+        <li id="code">Student Id: <%= student.get("id") %> </li>
+    </ul>
+    <br>
+    <table>
+        <% 
+            String[] weekDays = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"}; 
+            String[] times = {"7:30-9:00", "9:00-10:30", "10:30-12:00", "14:00-15:30", "16:00-17:30"};
+            ArrayList<HashMap<String, Object>> courses = (ArrayList<HashMap<String, Object>>) request.getAttribute("courses");
+        %>
+        <tr>
+            <th></th>
+            <% for (String time : times) { %>
+                <th>
+                    <%= time %>
+                </th>
+            <% } %>
+        </tr>
+
+        <%  %>
+        <% for (String day : weekDays) { %>
+            <tr>
+                <td><%= day %></td>
+                <% for (String time : times) { %>
+                    <td>
+                        <% for (HashMap<String, Object> c : courses) {
+                                HashMap<String, Object> cClassTime = (HashMap<String, Object>) c.get("classTime");
+                                ArrayList<String> cDays = (ArrayList<String>) cClassTime.get("days");
+                                String cTime = (String) cClassTime.get("time");
+                                if (cDays.contains(day) && cTime.equals(time)) { %>
+                                    <%= c.get("name") %>
+                                <% }%>
+                        <% } %>
+                    </td>
+                <% } %>
+            
+            </tr>
+        <% } %>
+
+    </table>
 </body>
 </html>
