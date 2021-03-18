1bb85c935a5e53448c6835ba7881381badf190ea
Sajjad <sssanikhani@gmail.com>
2021-03-18 15:44:45 +0330

    fix: check if student with login id exists

--- src/main/java/servlets/Login.java
+++ src/main/java/servlets/Login.java
@@ -1,15 +1,16 @@
 package servlets;
 
-import models.logic.DataBase;
-import models.logic.Handlers;
+import java.io.IOException;
 
+import javax.lang.model.util.ElementScanner6;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import models.logic.DataBase;
 
 @WebServlet(name = "LoginServlet", value = "/login")
 public class Login extends HttpServlet {
@@ -22,11 +23,11 @@ public class Login extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String studentId = request.getParameter("studentId");
-        if (studentId.equals("")) {
-            response.sendRedirect(request.getContextPath() + "/login");
-        } else {
+        if(DataBase.StudentManager.exists(studentId)) {
             DataBase.setLoggedInUserId(studentId);
-            response.sendRedirect(request.getContextPath() + "/");
+            response.sendRedirect("/");
+        } else {
+            response.sendRedirect(request.getServletPath());
         }
     }
 }
