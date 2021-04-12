44cb0191cb388cf9053073e4f08faba0187c5771
Sajjad <sssanikhani@gmail.com>
2021-04-12 13:37:40 +0430

    feat: add scheduler in server startup to perform SubmitWaitingListTask every 15 minutes

---
+++ src/main/java/servlets/BackgroundJobManager.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class BackgroundJobManager implements ServletContextListener {
+
+	private ScheduledExecutorService scheduler;
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		scheduler = Executors.newSingleThreadScheduledExecutor();
+		scheduler.scheduleAtFixedRate(new SubmitWaitingListTask(), 0, 15, TimeUnit.MINUTES);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		scheduler.shutdownNow();
+	}
+}
