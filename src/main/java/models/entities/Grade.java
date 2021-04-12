45451192bd904716df06424a648480c8e0751dc9
Sajjad <sssanikhani@gmail.com>
2021-04-12 15:40:34 +0430

    feat: add term attribute to Grade class

--- src/main/java/models/entities/Grade.java
+++ src/main/java/models/entities/Grade.java
@@ -4,20 +4,29 @@ public class Grade {
 
 	private String code;
 	private float grade;
+	private int term;
 
 	public String getCode() {
-		return code;
+		return this.code;
 	}
 
-	public void setCode(String code) {
-		this.code = code;
+	public void setCode(String _code) {
+		this.code = _code;
 	}
 
 	public float getGrade() {
-		return grade;
+		return this.grade;
 	}
 
-	public void setGrade(float grade) {
-		this.grade = grade;
+	public void setGrade(float _grade) {
+		this.grade = _grade;
+	}
+
+	public int getTerm() {
+		return this.term;
+	}
+
+	public void setTerm(int _term) {
+		this.term = _term;
 	}
 }
