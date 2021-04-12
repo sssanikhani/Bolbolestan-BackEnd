cc3d9c685f4cbf7aade6c3bc80aa4a6a53bb7aec
Sajjad <sssanikhani@gmail.com>
2021-04-12 13:27:59 +0430

    feat: add task for registering waiting lists of all offerings

---
+++ src/main/java/servlets/SubmitWaitingListTask.java
@@ -0,0 +1,17 @@
+package servlets;
+
+import java.util.ArrayList;
+
+import models.entities.Offering;
+import models.logic.DataBase;
+
+public class SubmitWaitingListTask implements Runnable {
+
+	@Override
+	public void run() {
+        ArrayList<Offering> offerings = DataBase.OfferingManager.getAll();
+        for (Offering o : offerings) {
+            o.registerWaitingStudents();
+        }
+    }
+}
