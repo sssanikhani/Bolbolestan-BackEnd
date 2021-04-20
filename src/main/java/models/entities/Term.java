e2a831c7cfb7d4d46d721d5870da65a45791e10c
Sajjad <sssanikhani@gmail.com>
2021-04-21 04:00:00 +0430

    feat: add getGrades method to Term class

--- src/main/java/models/entities/Term.java
+++ src/main/java/models/entities/Term.java
@@ -1,33 +1,39 @@
 package models.entities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Term {
-    private int term;
-    private HashMap<String, Grade> grades;
 
-    public int getTerm() {
-        return this.term;
-    }
+	private int term;
+	private HashMap<String, Grade> grades;
 
-    public int getUnits() {
-        int units = 0;
-        for (Grade g : this.grades.values()) {
-            units += g.getCourse().getUnits();
-        }
-        return units;
-    }
+	public int getTerm() {
+		return this.term;
+	}
 
-    public float getGpa() {
-        float total = 0;
-        for (Grade g : this.grades.values()) {
-            total += g.getGrade() * g.getCourse().getUnits();
-        }
-        return total / this.getUnits();
-    }
+	public int getUnits() {
+		int units = 0;
+		for (Grade g : this.grades.values()) {
+			units += g.getCourse().getUnits();
+		}
+		return units;
+	}
 
-    public void addGrade(Grade _grade) {
-        String code = _grade.getCourse().getCode();
-        this.grades.put(code, _grade);
-    }
+	public float getGpa() {
+		float total = 0;
+		for (Grade g : this.grades.values()) {
+			total += g.getGrade() * g.getCourse().getUnits();
+		}
+		return total / this.getUnits();
+	}
+
+	public ArrayList<Grade> getGrades() {
+		return (ArrayList<Grade>) this.grades.values();
+	}
+
+	public void addGrade(Grade _grade) {
+		String code = _grade.getCourse().getCode();
+		this.grades.put(code, _grade);
+	}
 }
