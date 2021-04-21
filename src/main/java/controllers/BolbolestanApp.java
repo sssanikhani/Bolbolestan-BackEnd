a131d5911a3676218389c3ae9fce3bc30071342a
Sajjad <sssanikhani@gmail.com>
2021-04-21 12:07:21 +0430

    feat: add spring application to project

---
+++ src/main/java/controllers/BolbolestanApp.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolbolestanApp {
+
+  public static void main(String... args) {
+    SpringApplication.run(BolbolestanApp.class, args);
+  }
+}
