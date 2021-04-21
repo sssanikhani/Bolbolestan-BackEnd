03e97e7f78963bc71fb47c4f8f409b7d5d4eded8
Sajjad <sssanikhani@gmail.com>
2021-04-21 21:57:55 +0430

    feat: return UnAuthorized response if student is not logged in

--- src/main/java/controllers/OfferingsController.java
+++ src/main/java/controllers/OfferingsController.java
@@ -3,6 +3,8 @@ package controllers;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import models.entities.Offering;
 import models.logic.DataBase;
 import models.serializers.OfferingSerializer;
+import models.statics.Responses;
 
 @RestController
 @RequestMapping("/offerings")
 public class OfferingsController {
     
     @GetMapping("")
-    public ArrayList<HashMap<String, Object>> getAll() {
+    public Object getAll(HttpServletResponse response) {
+        String loggedInUserId = DataBase.getLoggedInUserId();
+        if (loggedInUserId == null) {
+            response.setStatus(401);
+            return Responses.UnAuthorized;
+        }
         ArrayList<Offering> offerings = DataBase.OfferingManager.getAll();
-        ArrayList<HashMap<String, Object>> response = OfferingSerializer.serializeList(offerings);
-        return response;
+        ArrayList<HashMap<String, Object>> result = OfferingSerializer.serializeList(offerings);
+        return result;
     }
 }
