1831d8ba71998f20e5275323fe38f47e1aaaad59
Sajjad <sssanikhani@gmail.com>
2021-04-21 17:22:14 +0430

    feat: add ComponentScan annotation to BolbolestanApp to determine where components are

--- src/main/java/application/BolbolestanApp.java
+++ src/main/java/application/BolbolestanApp.java
@@ -2,11 +2,13 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "controllers", "application" })
 public class BolbolestanApp {
 
-  public static void main(String... args) {
-    SpringApplication.run(BolbolestanApp.class, args);
-  }
+	public static void main(String... args) {
+		SpringApplication.run(BolbolestanApp.class, args);
+	}
 }
