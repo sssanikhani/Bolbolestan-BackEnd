d5683df558495973987c2c030f5b5d91e229bff3
Sajjad <sssanikhani@gmail.com>
2021-04-21 12:49:31 +0430

    feat: add application directory to maintain application level files

--- src/main/java/application/StartupTasks.java
+++ src/main/java/application/StartupTasks.java
@@ -1,26 +1,24 @@
-package servlets;
+package application;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 import models.logic.DataBase;
 
-@WebListener
-public class StartupTasks implements ServletContextListener {
+@Component
+public class StartupTasks implements ApplicationRunner {
 
 	@Override
-	public void contextInitialized(ServletContextEvent sce) {
+	public void run(ApplicationArguments args) throws Exception {
 		try {
 			System.out.println("Trying to retrieve data from external DataBase...");
 			DataBase.OfferingManager.updateFromExternalServer();
 			DataBase.StudentManager.updateFromExternalServer();
+			System.out.println("All data have been received...");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("ERROR: failed to connect with external server");
 		}
 	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {}
 }
