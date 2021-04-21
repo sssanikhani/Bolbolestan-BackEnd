8be01be41badad1ce41c357a1af544cee23befae
Sajjad <sssanikhani@gmail.com>
2021-04-22 00:20:15 +0430

    feat: add GET /offerings/search endpoint

--- src/main/java/controllers/OfferingsController.java
+++ src/main/java/controllers/OfferingsController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import models.entities.Offering;
@@ -17,16 +18,27 @@ import models.statics.Responses;
 @RestController
 @RequestMapping("/offerings")
 public class OfferingsController {
-    
-    @GetMapping("")
-    public Object getAll(HttpServletResponse response) {
-        String loggedInUserId = DataBase.getLoggedInUserId();
-        if (loggedInUserId == null) {
-            response.setStatus(401);
-            return Responses.UnAuthorized;
-        }
-        ArrayList<Offering> offerings = DataBase.OfferingManager.getAll();
-        ArrayList<HashMap<String, Object>> result = OfferingSerializer.serializeList(offerings);
-        return result;
-    }
+
+	@GetMapping("")
+	public Object getAll(HttpServletResponse response) {
+		if (!DataBase.AuthManager.isLoggedIn()) {
+			response.setStatus(401);
+			return Responses.UnAuthorized;
+		}
+		ArrayList<Offering> list = DataBase.OfferingManager.getAll();
+		ArrayList<HashMap<String, Object>> result = OfferingSerializer.serializeList(list);
+		return result;
+	}
+
+	@GetMapping("/search")
+	public Object search(@RequestParam("q") String query, HttpServletResponse response) {
+		if (!DataBase.AuthManager.isLoggedIn()) {
+			response.setStatus(401);
+			return Responses.UnAuthorized;
+		}
+		System.out.println(query);
+		ArrayList<Offering> filtered = DataBase.OfferingManager.search(query);
+		ArrayList<HashMap<String, Object>> result = OfferingSerializer.serializeList(filtered);
+		return result;
+	}
 }
