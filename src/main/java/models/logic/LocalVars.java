5aafe999e9877eae9983ec979449964db616e84a
Sajjad <sssanikhani@gmail.com>
2021-05-02 13:17:10 +0430

    feat: add LocalVars module to get local environments needed in project

---
+++ src/main/java/models/logic/LocalVars.java
@@ -0,0 +1,19 @@
+package models.logic;
+
+public class LocalVars {
+    private static String dbUser = "#FILL";
+    private static String dbPassword = "#FILL";
+    private static String dbName = "#FILL";
+
+    public static String getDBUser() {
+        return dbUser;
+    }
+
+    public static String getDBPassword() {
+        return dbPassword;
+    }
+
+    public static String getDBName() {
+        return dbName;
+    }
+}
