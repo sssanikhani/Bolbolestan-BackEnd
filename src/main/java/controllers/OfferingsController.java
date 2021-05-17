0faf644e555f7c8abbfe504f4ab1de551fe517d6
Sajjad <sssanikhani@gmail.com>
2021-05-17 23:53:28 +0430

    fix: remove in server Authentication check in /offerings/* endpoints

--- src/main/java/controllers/OfferingsController.java
+++ src/main/java/controllers/OfferingsController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import controllers.responses.Responses;
-import models.database.DataBase;
 import models.database.repositories.OfferingRepository;
 import models.entities.Offering;
 import models.serializers.OfferingSerializer;
@@ -21,22 +20,18 @@ import models.serializers.OfferingSerializer;
 public class OfferingsController {
 
 	@GetMapping("")
-	public Object getAll(HttpServletResponse response) {
-		if (!DataBase.AuthManager.isLoggedIn()) {
-			response.setStatus(401);
-			return Responses.UnAuthorized;
-		}
+	public Object getAll(HttpServletRequest request, HttpServletResponse response) {
 		ArrayList<Offering> list = OfferingRepository.getAll();
 		ArrayList<HashMap<String, Object>> result = OfferingSerializer.serializeList(list);
 		return result;
 	}
 
 	@GetMapping("/search")
-	public Object search(@RequestParam("q") String query, HttpServletResponse response) {
-		if (!DataBase.AuthManager.isLoggedIn()) {
-			response.setStatus(401);
-			return Responses.UnAuthorized;
-		}
+	public Object search(
+		@RequestParam("q") String query,
+		HttpServletRequest request,
+		HttpServletResponse response
+	) {
 		ArrayList<Offering> filtered = OfferingRepository.search(query);
 		ArrayList<HashMap<String, Object>> result = OfferingSerializer.serializeList(filtered);
 		return result;
