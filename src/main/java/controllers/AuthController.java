f85f481942011c0498f2d5fd85d6399a1f6deb56
Sajjad <sssanikhani@gmail.com>
2021-05-17 19:27:10 +0430

    feat: add POST /auth/signup endpoint controller

--- src/main/java/controllers/AuthController.java
+++ src/main/java/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletResponse;
@@ -11,11 +12,74 @@ import org.springframework.web.bind.annotation.RestController;
 
 import controllers.responses.Responses;
 import models.database.DataBase;
+import models.database.repositories.StudentRepository;
+import models.entities.Student;
+import models.statics.Constants;
 import models.statics.Exceptions;
+import models.utils.Utils;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+	@PostMapping("/signup")
+	public HashMap<String, Object> signup(
+		@RequestBody HashMap<String, Object> requestBody,
+		HttpServletResponse response
+	) {
+		String[] requiredFields = {
+			"id", 
+			"email", 
+			"password",
+			"name",
+			"secondName",
+			"birthDate",
+			"faculty",
+			"field",
+			"level",
+			"status",
+			"img"
+		};
+		for (String key : requiredFields) {
+			if (!(requestBody.get(key) instanceof String)) {
+				response.setStatus(400);
+				return Responses.BadRequest;
+			}
+		}
+
+		String id = (String) requestBody.get("id");
+		String email = (String) requestBody.get("email");
+		boolean existId = StudentRepository.existsId(id);
+		boolean existEmail = StudentRepository.existsEmail(email);
+		if (existId || existEmail) {
+			response.setStatus(409);
+			return Responses.AlreadyExists;
+		}
+
+		Student newStudent = new Student();
+		newStudent.setId(id);
+		newStudent.setEmail(email);
+		newStudent.setPassword((String) requestBody.get("password"));
+		newStudent.setName((String) requestBody.get("name"));
+		newStudent.setSecondName((String) requestBody.get("secondName"));
+		newStudent.setBirthDate((String) requestBody.get("birthDate"));
+		newStudent.setFaculty((String) requestBody.get("faculty"));
+		newStudent.setField((String) requestBody.get("field"));
+		newStudent.setLevel((String) requestBody.get("level"));
+		newStudent.setStatus((String) requestBody.get("status"));
+		newStudent.setImg((String) requestBody.get("img"));
+		
+		ArrayList<Student> list = new ArrayList<>();
+		list.add(newStudent);
+		StudentRepository.bulkUpdate(list);
+
+		String jwtToken = Utils.createJWT(id, Constants.ONE_DAY);
+
+		HashMap<String, Object> res = new HashMap<>();
+		res.put("access", jwtToken);
+		response.setStatus(201);
+		return res;
+	}
 
 	@PostMapping("/login")
 	public HashMap<String, Object> login(
@@ -48,9 +112,4 @@ public class AuthController {
 		return Responses.OK;
 	}
 
-	@PostMapping("/logout")
-	public HashMap<String, Object> logout() {
-		DataBase.AuthManager.logout();
-		return Responses.OK;
-	}
 }
