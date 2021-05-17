e12ea610c018df144aa13e487e3e253a63573f4d
Sajjad <sssanikhani@gmail.com>
2021-05-17 21:53:05 +0430

    fix: refactor JWTFilter and handle not existing authorization header

--- src/main/java/controllers/filters/JWTFilter.java
+++ src/main/java/controllers/filters/JWTFilter.java
@@ -8,60 +8,64 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Component;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import models.database.repositories.StudentRepository;
-import models.entities.Student;
-import models.statics.Exceptions;
+import models.database.LocalVars;
 
-@Component
+@WebFilter(filterName = "JWTFilter", urlPatterns = { "/student/*", "/offerings/*" })
 public class JWTFilter implements Filter {
-    @Override
-    public void doFilter(
-            ServletRequest servletRequest,
-            ServletResponse servletResponse,
-            FilterChain chain
-    ) throws IOException, ServletException {
+
+	@Override
+	public void doFilter(
+		ServletRequest servletRequest,
+		ServletResponse servletResponse,
+		FilterChain chain
+	)
+		throws IOException, ServletException {
+        
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        String jwt = request.getHeader("jwtToken");
-//        System.out.println("CORSFilter HTTP Request: " + request.getHeader("jwtToken"));
-        Claims claims = decodeJWT(jwt);
-        Student s = null;
-        //IS NULL
-        if(claims == null){
-            response.setStatus(401);
-//            chain.doFilter(request, response);
-        } else {
-            long nowMillis = System.currentTimeMillis();
-            Date now = new Date(nowMillis);
-            if(now.compareTo(claims.getExpiration())>0){
-                try {
-                    s = StudentRepository.get((String) claims.get("id"), false);
-                } catch (Exceptions.StudentNotFound e) {
-                    response.setStatus(403);
-//                    chain.doFilter(request, response);
-                }
-            } else {
-                response.setStatus(403);
-//                chain.doFilter(request, response);
-            }
-        }
-        servletRequest.setAttribute("student", s);
-        chain.doFilter(servletRequest,servletResponse);
-    }
+		String authHeader = request.getHeader("Authorization");
+		if (authHeader == null) {
+			response.setStatus(401);
+			return;
+		}
 
-    public Claims decodeJWT(String token) {
-        return Jwts.parser()
-                .setSigningKey("bolbolestan")
-                .parseClaimsJws(token).getBody();
-    }
-    @Override
-    public void destroy() {}
+		String jwt = authHeader.substring(7); // without Bearer
+
+		Claims claims = decodeJWT(jwt);
+
+		if (claims == null) {
+			response.setStatus(401);
+			return;
+		}
+
+		long nowMillis = System.currentTimeMillis();
+		Date now = new Date(nowMillis);
+		Date exp = claims.getExpiration();
+		if (exp.compareTo(now) < 0) {
+			response.setStatus(403);
+			return;
+		}
+
+		String id = (String) claims.get("id");
+		servletRequest.setAttribute("id", id);
+		chain.doFilter(servletRequest, servletResponse);
+	}
+
+	public Claims decodeJWT(String token) {
+		return Jwts
+			.parser()
+			.setSigningKey(LocalVars.secretKey)
+			.parseClaimsJws(token)
+			.getBody();
+	}
+
+	@Override
+	public void destroy() {}
 }
