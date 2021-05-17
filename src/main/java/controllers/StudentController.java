4f3b9916953ccac8f2e61a40a7a1944d7baa65cb
Sajjad <sssanikhani@gmail.com>
2021-05-18 00:03:24 +0430

    feat: get Student object from request instead of AuthManager

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -3,14 +3,13 @@ package controllers;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import controllers.responses.Responses;
-import models.database.DataBase;
 import models.entities.Offering;
 import models.entities.Student;
 import models.entities.Term;
@@ -23,38 +22,32 @@ import models.serializers.TermReportSerializer;
 public class StudentController {
 
 	@GetMapping("")
-	public HashMap<String, Object> getStudentInfo(HttpServletResponse response) {
-		if (!DataBase.AuthManager.isLoggedIn()) {
-			response.setStatus(401);
-			return Responses.UnAuthorized;
-		}
+	public HashMap<String, Object> getStudentInfo(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) {
+		Student s = (Student) request.getAttribute("student");
 
-		Student s = DataBase.AuthManager.getLoggedInUser();
 		HashMap<String, Object> result = StudentSerializer.serialize(s);
 		return result;
 	}
 
 	@GetMapping("/report-card")
-	public Object getStudentReportCard(HttpServletResponse response) {
-		if (!DataBase.AuthManager.isLoggedIn()) {
-			response.setStatus(401);
-			return Responses.UnAuthorized;
-		}
+	public Object getStudentReportCard(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) {
+		Student s = (Student) request.getAttribute("student");
 
-		Student s = DataBase.AuthManager.getLoggedInUser();
 		ArrayList<Term> tList = s.getTermsReport();
 		ArrayList<HashMap<String, Object>> result = TermReportSerializer.serializeList(tList);
 		return result;
 	}
 
 	@GetMapping("/plan")
-	public Object getStudentPlan(HttpServletResponse response) {
-		if (!DataBase.AuthManager.isLoggedIn()) {
-			response.setStatus(401);
-			return Responses.UnAuthorized;
-		}
+	public Object getStudentPlan(HttpServletRequest request, HttpServletResponse response) {
+		Student s = (Student) request.getAttribute("student");
 
-		Student s = DataBase.AuthManager.getLoggedInUser();
 		HashMap<String, ArrayList<Offering>> plan = s.getPlan();
 		HashMap<String, ArrayList<HashMap<String, Object>>> result = new HashMap<>();
 		for (String day : plan.keySet()) {
