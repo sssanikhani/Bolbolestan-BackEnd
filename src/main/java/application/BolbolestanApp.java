7bb3414cbba08ae80182292533b439cf9d4df3d5
Sajjad <sssanikhani@gmail.com>
2021-05-17 21:48:48 +0430

    feat: add ServletComponentScan feature to Spring application

--- src/main/java/application/BolbolestanApp.java
+++ src/main/java/application/BolbolestanApp.java
@@ -2,9 +2,11 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = { "controllers", "application" })
 @ComponentScan(basePackages = { "controllers", "application" })
 public class BolbolestanApp {
 
