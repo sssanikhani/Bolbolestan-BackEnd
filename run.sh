65a91bce404dbc82020adabd09dde13a36fcec94
Sajjad <sssanikhani@gmail.com>
2021-05-27 03:52:54 +0430

    feat: start containers if already exist and are stopped

--- run.sh
+++ run.sh
@@ -12,7 +12,11 @@ docker network create $network_name
 docker run --network $network_name --name $mysql_name -it \
     -e MYSQL_ROOT_PASSWORD="root" -e MYSQL_DATABASE=$dbname -d mysql:8
 
+docker start $mysql_name
+
 docker build -t $project_image_name .
 
 [[ $(docker ps -f "name=$project_name" --format '{{.Names}}') == $project_name ]] || 
 docker run --network $network_name --name $project_name -p 8080:8080 -it $project_image_name
+
+docker start $project_name
