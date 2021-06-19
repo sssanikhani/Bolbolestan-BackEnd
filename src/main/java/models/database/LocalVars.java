866464afa6c2e25a12fc757c9bbb4c14702108ca
Sajjad <sssanikhani@gmail.com>
2021-06-19 17:51:29 +0430

    feat: get db credentials from env vars

--- src/main/java/models/database/LocalVars.java
+++ src/main/java/models/database/LocalVars.java
@@ -1,8 +1,8 @@
 package models.database;
 
 public class LocalVars {
-    public static final String dbUser = "root";
-    public static final String dbPassword = "root";
+    public static final String dbUser = System.getenv("DB_USERNAME");
+    public static final String dbPassword = System.getenv("DB_PASSWORD");
     public static final String dbName = "bolbolestan";
     public static final String secretKey = "bolbolestan";
 }
